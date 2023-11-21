317fd19477fa5af784e0c2473a292b195ec11f95
bohun <slawkd2002@gmail.com>
2023-11-22 01:08:53 +0900

    Comment:JumpKing_Goal

--- zzapRio/Assets/Scripts/JumpKing_Goal.cs
+++ zzapRio/Assets/Scripts/JumpKing_Goal.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 
 public class JumpKing_Goal : MonoBehaviour
 {
-    [SerializeField] float rotationSpeed;
+    [SerializeField] float rotationSpeed; // 회전 속도
     private void Update()
     {
-        rotationSpeed = Mathf.PingPong(Time.time*5f, 2f) + 0.5f;
-        transform.Rotate(new Vector3(0, 0, rotationSpeed));
+        rotationSpeed = Mathf.PingPong(Time.time*5f, 1.5f) + 0.5f; // 0.5 ~ 2.5까지 반복해서 값을가지는 속도
+        transform.Rotate(new Vector3(0, 0, rotationSpeed));// 회전
     }
 }
