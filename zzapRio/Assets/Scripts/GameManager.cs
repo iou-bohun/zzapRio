9b246eab715d9ef00d885f5f07373126c8f5de96
bohun <slawkd2002@gmail.com>
2023-11-21 14:52:28 +0900

    feat: 매니저 스크립트 작성
    1.GameManager 싱글톤
    2.DataManager  싱글톤
    3.SoundManager 싱글톤

--- zzapRio/Assets/Scripts/GameManager.cs
+++ zzapRio/Assets/Scripts/GameManager.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static GameManager instance;
+    public static GameManager Instance
     {
-        
+        get
+        {
+            if (null == instance)
+            {
+                return null;
+            }
+            else
+                return instance;
+        }    
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else if(instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
     }
 }
