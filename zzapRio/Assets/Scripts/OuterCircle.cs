2abdec2e8a658360b23e06dbc2f8a4a8e41710e9
bohun <slawkd2002@gmail.com>
2023-11-23 22:37:43 +0900

    feat:격돌 게임 로직
    1. 키 입력
    2. 키 입력 타이밍 로직

--- zzapRio/Assets/Scripts/OuterCircle.cs
+++ zzapRio/Assets/Scripts/OuterCircle.cs
@@ -1,14 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor.U2D;
 using UnityEngine;
 
 public class OuterCircle : MonoBehaviour
 {
     [SerializeField] float speed;
+    [SerializeField] bool isPerfect;
+    
     private void Update()
     {
-        transform.localScale = new Vector3(transform.localScale.x - 1f * speed * Time.deltaTime, 
+        UpdateSacle();
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            if (isPerfect)
+            {
+                Debug.Log("Win");
+            }
+            else
+            {
+                Debug.Log("Lose");
+            }
+        }
+    }
+
+
+
+    void UpdateSacle()
+    {
+        transform.localScale = new Vector3(transform.localScale.x - 1f * speed * Time.deltaTime,
             transform.localScale.y - 1f * speed * Time.deltaTime, 0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        isPerfect = true;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isPerfect = false;
     }
 }
