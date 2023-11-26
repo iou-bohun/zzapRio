cdbc26fac00b30c4ed462fe6faf0d80a56513275
leejh0469 <leejh0469@naver.com>
2023-11-26 23:14:05 +0900

    feat: 도로 스프라이트 제작 etc
    1. 도로 스프라이트 제작 및 collider 추가
    2. 충돌 시 GameOver상태로 전환

---
+++ zzapRio/Assets/Scripts/RoadRacing_PlayerMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadRacing_PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    public float speed;
+
+    public bool gameOver = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!gameOver)
+        {
+            transform.Translate(Vector3.right * speed * Input.GetAxis("Mouse X") * Time.deltaTime);
+        }
+    }
+}
