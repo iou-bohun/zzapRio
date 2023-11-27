73f4710c6cd0e0f3d778e3321f9d41c1eea92863
bohun <56661597+iou-bohun@users.noreply.github.com>
2023-11-27 15:44:28 +0900

    Revert "feat: 애니메이션추가"

--- zzapRio/Assets/Scripts/GameManager.cs
+++ zzapRio/Assets/Scripts/GameManager.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.SearchService;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
