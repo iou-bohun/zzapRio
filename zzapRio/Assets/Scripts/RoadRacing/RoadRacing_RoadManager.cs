62e7b00a62236eb820ebf5a2f97e44121cabf95b
leejh0469 <leejh0469@naver.com>
2023-11-28 19:03:58 +0900

    comment: ClawMachine, RoadRacing, Subrunker에서 작업한 스크립트 주석 추가

--- zzapRio/Assets/Scripts/RoadRacing/RoadRacing_RoadManager.cs
+++ zzapRio/Assets/Scripts/RoadRacing/RoadRacing_RoadManager.cs
@@ -17,7 +17,10 @@ public class RoadRacing_RoadManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // road 오브젝트 위치 조정
         roads.transform.position = startPos;
+
+        // 현제 Road 오브젝트가 다 내려온 후 다음 오브젝트를 이어 붙여 내려주기 위해 현재, 다음 Road 오브젝트 저장.
         currentRoad = roads;
         nextRoad = roads;
     }
@@ -31,6 +34,7 @@ public class RoadRacing_RoadManager : MonoBehaviour
         }
         else
         {
+            // Road 오브젝트를 끝지점까지 내리면 다음 Road 오브젝트를 이어 내려주기
             currentRoad = nextRoad;
             currentRoad.transform.position = startPos;
         }
--- zzapRio/Assets/Scripts/Subrunker/Subrunker_Bullet.cs
+++ zzapRio/Assets/Scripts/Subrunker/Subrunker_Bullet.cs
@@ -18,6 +18,7 @@ public class Subrunker_Bullet : MonoBehaviour
 
         if (plyaer != null)
         {
+            // Player 오브젝트를 바라보는 방향벡트 구하기
             dircVector = (plyaer.transform.position - transform.position).normalized;
         }
     }
@@ -32,6 +33,7 @@ public class Subrunker_Bullet : MonoBehaviour
     {
         if (collision.gameObject.name == "Player")
         {
+            // Player 오브젝트에 충돌했을 때 실행. 충돌시 GameOver 상태로 전환하면 될 듯
             Debug.Log("hit");
         }
     }
